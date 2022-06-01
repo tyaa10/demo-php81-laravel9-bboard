8742a0d144885d0afd30e1886ba84419611678e2
YuriiTrofimenko <tyaa@ukr.net>
2022-06-01 07:59:02 +0300

    bb model, migration and action logic in the controller

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bb;
 use Illuminate\Http\Request;
 
 class BbsController extends Controller
 {
     public function index() {
-        return response('Здесь будет перечень объявлений.')
+        $bbs = Bb::latest()->get();
+        $s = "Объявления\r\n\r\n";
+        foreach ($bbs as $bb) {
+            $s .= $bb->title . "\r\n";
+            $s .= $bb->price . " руб.\r\n";
+            $s .= "\r\n";
+        }
+        return response($s)
             ->header('Content-Type', 'text/plain');
     }
 }
