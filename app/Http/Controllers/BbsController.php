18825a960bd8376101dd04f74e5d48f7c64eab94
YuriiTrofimenko <tyaa@ukr.net>
2022-06-01 18:37:27 +0300

    views: bb list -> details

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -8,21 +8,10 @@ use Illuminate\Http\Request;
 class BbsController extends Controller
 {
     public function index() {
-        $bbs = Bb::latest()->get();
-        $s = "Объявления\r\n\r\n";
-        foreach ($bbs as $bb) {
-            $s .= $bb->title . "\r\n";
-            $s .= $bb->price . " руб.\r\n";
-            $s .= "\r\n";
-        }
-        return response($s)
-            ->header('Content-Type', 'text/plain');
+        $context = ['bbs' => Bb::latest()->get()];
+        return view('index', $context);
     }
     public function detail(Bb $bb) {
-        // $bb = Bb::find($bb);
-        $s = $bb->title . "\r\n\r\n";
-        $s .= $bb->content . "\r\n";
-        $s .= $bb->price . " руб.\r\n";
-        return response($s)->header('Content-Type', 'text/plain');
+        return view('detail', ['bb' => $bb]);
     }
 }
